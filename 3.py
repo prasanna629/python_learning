2ea08916d2f01465e6442e1a8cd4a17b5c0fd6ec
prasanna629 <prasanna.160403@gmail.com>
2025-04-29 20:49:47 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+course = "python course"
+print(len(course))
+print(course[0])
+print(course[-2])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
---
+++ 4.py
@@ -0,0 +1,9 @@
+course = "python course"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.rstrip())
+print(course.lstrip())
+print(course.find("Pro"))
+print(course.replace("p","j"))
