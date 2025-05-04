abb789018f87d11bf04714bfcdd8df158a1dcc27
prasanna629 <prasanna.160403@gmail.com>
2025-05-04 10:15:14 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,50 @@
+#str = "Hello, World!"
+# print(str[0])
+# print(str[6])
+
+
+
+
+# str = "GeeksforGeeks"
+# print(str[3:])
+# print(str[3:8])
+# print(str[:3])
+# print(str[3:8:2])
+
+
+
+# str ="greeksforGeeks"
+# str ="G" +  str[1:]
+# print(str)
+
+
+# s = "hello geeks"
+# s1 ="H" + s[1:]
+# s2 = s.replace("geeks", "GeekforGeeks")
+# print(s1)
+# print(s2)
+
+
+
+
+
+# s= "prasanna"
+# s1 ="P" + s[1:]
+# s2 = s.replace("prasanna", "gantlana")
+# print(s1)
+# print(s2)
+
+
+# s ="ABC"
+# print(s.strip())
+# s ="XYZ is a programmin language"
+# print(s.replace("XYZ", "ABC"))
+
+
+
+#name = "johne doe"
+#age = 25
+#height = 5.9
+#print(f"Name: {name},Age:{age},Height:{height}")
+
+
