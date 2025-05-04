3f0d720f077280b92f5a9d4db65050c6a8b14ce7
prasanna629 <prasanna.160403@gmail.com>
2025-05-04 11:17:24 +0530

    Add files via upload

---
+++ s.py
@@ -0,0 +1,41 @@
+# name_1 = "prasanna"
+# name_2 = "R" + name_1[2:]
+# print("name_1 = ", name_1, "name_2 = ", name_2)
+
+
+
+
+# my_string = "Hello"
+# new_string = my_string + ",prasanna"
+# print("This is a new string")
+# print(new_string)
+
+
+
+#s1 = "abcd"
+#s2 = ""
+#s3 = "e"
+#print(len(s1))
+#print(len(s2))
+#print(len(s3))
+
+
+
+
+
+# s = " gantlanaprasanna"
+# print(s[-4:])
+# print(s[:-3])
+# print(s[-4:-2])
+# print(s[-8:-1:2])
+
+
+
+# str = "prasanna"
+# print(str[::-1])
+
+s = " Hello ,World!"
+s2 = s[:]
+s3= s[::]
+print(s2,s3)
+
