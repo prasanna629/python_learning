aaab7f67f2fcbe6141a200e3928b8d26ac482de9
prasanna629 <prasanna.160403@gmail.com>
2025-05-06 19:28:04 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,64 @@
+#def func():
+ #   print("This is function")
+#func()
+
+
+#def hello():
+ #   print("hello everyone")
+#hello()
+#print("end of the program")
+
+
+
+
+#def add(num1,num2):
+ #   sum = num1 + num2
+  #  print("sum of 2 number:",sum)
+#add(5,8)
+#print("end of the program")
+
+
+
+
+
+
+#def func(a,b,c):
+ #   print("This is function",a,b,c)
+#func(1,2,3)
+
+
+
+#def outer():
+ #   print("outer function")
+  #  def inner():
+   #     print("inner function")
+    #inner()
+#outer()
+
+
+
+#def subNumbers(x, y):
+ #   return (x-y)
+#a = 90
+#b = 50
+#res = subNumbers(a, b)
+#print("subtraction of ", a, " and ", b, " is ", res)
+
+
+
+
+def fun(func, arg):
+    return func(arg)
+def square(x):
+    return x ** 2
+res = fun(square, 5)
+print(res)
+
+
+
+
+
+#def fun(*args):
+ #   for arg in args:
+  #      print(arg)
+#fun(1, 2, 3, 4, 5)
---
+++ var.py
@@ -0,0 +1,56 @@
+#a = 15
+#b = 4
+#print("Addition:", a + b)  
+#print("Subtraction:", a - b) 
+#print("Multiplication:", a * b)  
+#print("Division:", a / b) 
+#print("Modulus:", a % b) 
+
+
+
+
+
+#a = 13
+#b = 33
+#print(a > b)
+#print(a < b)
+#print(a == b)
+#print(a != b)
+#print(a >= b)
+#print(a <= b)
+
+
+
+#a = True
+#b = False
+#print(a and b)
+#print(a or b)
+#print(not a)
+
+
+
+
+#a = 10
+#b = 4
+#print(a & b)
+#print(a | b)
+#print(~a)
+#print(a ^ b)
+#print(a >> 2)
+#print(a << 2)
+
+
+
+
+
+a = 10
+b = a
+print(b)
+b += a
+print(b)
+b -= a
+print(b)
+b *= a
+print(b)
+b <<= a
+print(b)
