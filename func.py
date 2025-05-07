76a04959eb3e0c2769f9bd860365620389844c82
prasanna629 <prasanna.160403@gmail.com>
2025-05-07 20:20:27 +0530

    Add files via upload

--- func.py
+++ func.py
@@ -47,12 +47,12 @@
 
 
 
-def fun(func, arg):
-    return func(arg)
-def square(x):
-    return x ** 2
-res = fun(square, 5)
-print(res)
+#def fun(func, arg):
+ #   return func(arg)
+#def square(x):
+ #   return x ** 2
+#res = fun(square, 5)
+#print(res)
 
 
 
@@ -67,10 +67,65 @@ print(res)
 
 
 
-def evenOdd(x):
-    if (x % 2 == 0):
-        print("even")
-    else:
-        print("odd")
-evenOdd(2)
-evenOdd(3)
+#def evenOdd(x):
+    #if (x % 2 == 0):
+   #     print("even")
+  #  else:
+ #       print("odd")
+#evenOdd(2)
+#evenOdd(3)
+
+
+
+
+#def fun():
+#    print("Welcome to GFG")
+#fun()
+
+
+#var = -94
+#print('Absolute value of integer is:', abs(var))
+
+
+
+
+#float_number = -54.26
+#print('Absolute value of float is:', 
+               #   abs(float_number))
+
+
+#l = [False, False, True, False, False]
+#print(any(l))
+
+
+
+
+
+#s = "Hi There!"
+#print(any(s))
+#s = "000"
+#print(any(s))
+#s = ""
+#print(any(s))
+
+
+
+
+
+def cal_dis(speed, time):
+    print(" Time(hr) :", time)
+    print(" Speed(km / hr) :", speed)
+    return speed * time
+
+def cal_time(dist, speed):
+    print(" Distance(km) :", dist)
+    print(" Speed(km / hr) :", speed)
+    return dist / speed
+print(" The calculated Speed(km / hr) is :", cal_speed(abs(45.9), abs(-2)))
+print("")
+print(" The calculated Distance(km) :",
+      cal_dis(abs(-62.9), abs(2.5)))
+print("")
+
+print(" The calculated Time(hr) :",
+      cal_time(abs(48.0), abs(4.5)))
