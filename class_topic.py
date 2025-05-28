d7dd93186f6b52e15f4765755158d2f7020d72a9
prasanna629 <prasanna.160403@gmail.com>
2025-05-28 19:37:55 +0530

    Add files via upload

---
+++ class_topic.py
@@ -0,0 +1,97 @@
+# class python():
+#     a=3
+#     print(a)
+
+# class MyClass:
+#   x = 5
+
+# class MyClass:
+#     x=5
+# p1 = MyClass()
+# print(p1.x)
+
+
+
+# class python():
+#     a=2
+#     def output(self):
+#          print(self.a)
+# b=python()
+# v=python()
+# b.output()
+# v.output()
+
+
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+# p1 = Person("Abcd", 86)
+# print(p1.name)
+# print(p1.age)
+
+
+
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+# p1 = Person("ABCDE", 36)
+# print(p1)
+
+
+
+# class Dog:
+#     sound = "bark" 
+# dog1 = Dog()
+# print(dog1.sound)
+
+
+
+# class Dog:
+#     species = "Canine"  
+#     def __init__(self, name, age):
+#         self.name = name  
+#         self.age = age  
+
+
+
+
+
+# class Dog:
+#     species = "Canine" 
+#     def __init__(self, name, age):
+#         self.name = name 
+#         self.age = age  
+# dog1 = Dog("Buddy", 3)
+# print(dog1.name)  
+# print(dog1.species) 
+
+
+
+
+# class Dog:
+#     species = "Canine"
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+# dog1 = Dog("Buddy", 3)
+# dog2 = Dog("Charlie", 5)
+# print(dog1.species)  
+# print(dog1.name)     
+# print(dog2.name)     
+# dog1.name = "Max"
+# print(dog1.name)     
+# Dog.species = "Feline"
+# print(dog1.species) 
+# print(dog2.species)
+
+
+
+
+
+
+
+
